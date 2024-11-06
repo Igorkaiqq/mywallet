112e6eae8fdb4663014bf568017f1cb3b2584290
Gabriel <gabrielheidrich2@hotmail.com>
2024-11-05 21:13:03 -0300

    front att 2

--- Front/src/app/wallet-app/wallet-app.component.ts
+++ Front/src/app/wallet-app/wallet-app.component.ts
@@ -1,81 +1,95 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Chart } from 'chart.js';
-import { ApiService } from '../../app/service/graficos/api.service';
+import {TransacaoService} from "../service/transacao/transacao.service";
+import {CommonModule, CurrencyPipe, DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-wallet',
+  standalone: true,
+  imports: [ReactiveFormsModule, CurrencyPipe, DatePipe, CommonModule],
   templateUrl: './wallet-app.component.html',
   styleUrls: ['./wallet-app.component.css']
 })
 export class WalletAppComponent implements OnInit {
-  public receitaChart: any;
-  public despesaChart: any;
+  walletAppForm = new FormGroup({
+    saldo: new FormControl(15000.00, Validators.required),
+    receitas: new FormControl(8000.00, Validators.required),
+    despesas: new FormControl(3000.00, Validators.required)
+  });
 
-  constructor(private apiService: ApiService, private router: Router) {}
+  transacoes: any[] = [];
+
+  categoriasDespesas = [
+    { nome: 'Moradia', valor: 1200.00, icone: 'bi-house' },
+    { nome: 'Cartão', valor: 800.00, icone: 'bi-credit-card' },
+    { nome: 'Transporte', valor: 500.00, icone: 'bi-car-front' },
+    { nome: 'Lazer', valor: 400.00, icone: 'bi-tree' }
+  ];
+
+  topDespesas = [
+    { nome: 'Aluguel', valor: 1200.00 },
+    { nome: 'Mercado', valor: 600.00 },
+    { nome: 'Transporte', valor: 300.00 },
+    { nome: 'Entretenimento', valor: 200.00 },
+    { nome: 'Academia', valor: 100.00 }
+  ];
+
+  topReceitas = [
+    { nome: 'Salário', valor: 5000.00 },
+    { nome: 'Freelance', valor: 2000.00 },
+    { nome: 'Investimentos', valor: 1000.00 }
+  ];
 
   ngOnInit(): void {
-    this.initReceitaChart();
-    this.initDespesaChart();
+    this.buscarTransacoes();
   }
 
-  // Método para navegação para a página de realizar transação
+<<<<<<< main
+  onSubmit() {
+    if (this.walletAppForm.valid) {
+      console.log('Form Data: ', this.walletAppForm.value);
+
+    } else {
+      console.log('Form inválido');
+    }
+=======
   goToCadTransacao() {
     this.router.navigate(['/realizar-transacao']);
+>>>>>>> local
   }
 
-  goToMovimentacao(){
-    this.router.navigate(['/movimentacoes']);
+  onCancel() {
+    this.walletAppForm.reset();
   }
 
-  // Método para navegação para a página de login
+<<<<<<< main
+  constructor(
+    private router: Router,
+    private transacaoService: TransacaoService
+  ) {}
+
+  goToCadTransacao() {
+    this.router.navigate(['/realizar-transacao']);
+=======
   goLogin() {
+    this.router.navigate(['']);
+>>>>>>> local
+  }
+
+  goLogin(){
     this.router.navigate(['']);
   }
 
-  private initReceitaChart(): void {
-    this.apiService.getReceitas().subscribe(data => {
-      this.receitaChart = new Chart('graficoReceitasCanvas', {
-        type: 'bar',
-        data: {
-          labels: data.map((item: any) => item.nome),
-          datasets: [
-            {
-              label: 'Receitas',
-              data: data.map((item: any) => item.valor),
-              backgroundColor: 'rgba(0, 123, 255, 0.5)',
-              borderColor: 'rgba(0, 123, 255, 1)',
-              borderWidth: 1
-            }
-          ]
-        },
-        options: {
-          responsive: true
-        }
-      });
-    });
-  }
+  buscarTransacoes() {
 
-  private initDespesaChart(): void {
-    this.apiService.getDespesas().subscribe(data => {
-      this.despesaChart = new Chart('graficoDespesasCanvas', {
-        type: 'line',
-        data: {
-          labels: data.map((item: any) => item.nome),
-          datasets: [
-            {
-              label: 'Despesas',
-              data: data.map((item: any) => item.valor),
-              backgroundColor: 'rgba(255, 99, 132, 0.5)',
-              borderColor: 'rgba(255, 99, 132, 1)',
-              borderWidth: 1
-            }
-          ]
+      this.transacaoService.buscarTransacoesPorUsuarioId().subscribe({
+        next: (transacoes) => {
+          this.transacoes = transacoes;
         },
-        options: {
-          responsive: true
+        error: (error) => {
+          console.error('Erro ao buscar transações: ', error);
         }
       });
-    });
-  }
+    }
 }
