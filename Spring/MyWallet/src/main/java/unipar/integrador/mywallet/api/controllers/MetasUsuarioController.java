f7152cadabcd6c31efc1590eb401d49f7df4e3ea
Igor Kaique <igor.oliveira@treetech.com.br>
2024-11-18 21:28:59 -0300

    correção

--- Spring/MyWallet/src/main/java/unipar/integrador/mywallet/api/controllers/MetasUsuarioController.java
+++ Spring/MyWallet/src/main/java/unipar/integrador/mywallet/api/controllers/MetasUsuarioController.java
@@ -1,38 +1,48 @@
 package unipar.integrador.mywallet.api.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import unipar.integrador.mywallet.application.dto.metas.MetasUsuarioDTO;
+import unipar.integrador.mywallet.application.entities.MetasUsuarioEntity;
+import unipar.integrador.mywallet.application.interfaces.IMetasUsuario;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/metas-usuario")
 public class MetasUsuarioController {
 
-//    @Autowired
-//    private IMetasUsuario metasService;
-//
-//    @PostMapping
-//    public ResponseEntity<MetasUsuarioEntity> create(@Valid @RequestBody MetasUsuarioDTO dto) {
-//        MetasUsuarioEntity metas = metasService.save(dto);
-//        return new ResponseEntity<>(metas, HttpStatus.CREATED);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<MetasUsuarioEntity> findById(@PathVariable UUID id) {
-//        Optional<MetasUsuarioEntity> metas = metasService.findById(id);
-//        return metas.map(ResponseEntity::ok)
-//                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<List<MetasUsuarioEntity>> findAll() {
-//        List<MetasUsuarioEntity> metas = metasService.findAll();
-//        return new ResponseEntity<>(metas, HttpStatus.OK);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<MetasUsuarioEntity> update(@PathVariable UUID id, @Valid @RequestBody MetasUsuarioDTO dto) {
-//        MetasUsuarioEntity metas = metasService.update(id, dto);
-//        return new ResponseEntity<>(metas, HttpStatus.OK);
-//    }
+    @Autowired
+    private IMetasUsuario metasService;
+
+    @PostMapping
+    public ResponseEntity<MetasUsuarioEntity> create(@Valid @RequestBody MetasUsuarioDTO dto) {
+        MetasUsuarioEntity metas = metasService.save(dto);
+        return new ResponseEntity<>(metas, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MetasUsuarioEntity> findById(@PathVariable UUID id) {
+        Optional<MetasUsuarioEntity> metas = metasService.findById(id);
+        return metas.map(ResponseEntity::ok)
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MetasUsuarioEntity>> findAll() {
+        List<MetasUsuarioEntity> metas = metasService.findAll();
+        return new ResponseEntity<>(metas, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MetasUsuarioEntity> update(@PathVariable UUID id, @Valid @RequestBody MetasUsuarioDTO dto) {
+        MetasUsuarioEntity metasUpdt = metasService.update(id, dto);
+        return new ResponseEntity<>(metasUpdt, HttpStatus.OK);
+    }
 }
