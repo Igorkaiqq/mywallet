e772c0b42d09f6d1d611f38291c7fe622f12f607
Gabriel <gabrielheidrich2@hotmail.com>
2024-11-19 19:33:10 -0300

    att. metas back

---
+++ Front/src/app/overlay/criar-metas/criar-metas.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CriarMetasComponent } from './criar-metas.component';
+
+describe('CriarMetasComponent', () => {
+  let component: CriarMetasComponent;
+  let fixture: ComponentFixture<CriarMetasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CriarMetasComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CriarMetasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
